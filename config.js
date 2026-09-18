df34cb31b0c6f5d1118aeb7f0c1296d3c3094c03
nyedotgit <nyedotmovie2@gmail.com>
2026-09-18 10:42:11 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Public Google Apps Script Web App address. No private key is needed.
+window.APP_CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzIYUDU5Mbtdm78rwL0nJVkpeCqHctJFTyLlBPnjQQDe731OF9MhaSiOh9YN_rTmNnM-w/exec'
+};
