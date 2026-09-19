509626bba5eae44442f289e5a9ed048edcc22c52
nyedotgit <nyedotmovie2@gmail.com>
2026-09-19 10:14:53 +0700

    Revise comment on GAS_URL access keys
    Updated comment to emphasize keeping access keys private.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// Public Google Apps Script Web App address. No private key is needed.
+// Public Google Apps Script Web App address. Keep both access keys out of this public file.
 window.APP_CONFIG = {
   GAS_URL: 'https://script.google.com/macros/s/AKfycbzIYUDU5Mbtdm78rwL0nJVkpeCqHctJFTyLlBPnjQQDe731OF9MhaSiOh9YN_rTmNnM-w/exec'
 };
